e60c1c80139c7f8f081f3c255bed08508a7835bf
zzoon20 <zzoon20@218.38.137.28>
2016-08-23 13:35:46 +0900

    QnaServiceTest 구현

---
+++ src/test/java/core/test/QnaServiceTest.java
@@ -0,0 +1,56 @@
+package core.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import next.CannotOperateException;
+import next.dao.AnswerRepository;
+import next.dao.QuestionRepository;
+import next.dao.UserRepository;
+import next.model.Answer;
+import next.model.Question;
+import next.model.User;
+import next.service.QnaService;
+
+
+public class QnaServiceTest extends IntegrationTest{
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private QuestionRepository questionRepository;
+	@Autowired
+	private AnswerRepository answerRepository;
+	private QnaService qnaService;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		qnaService = new QnaService(questionRepository, answerRepository);
+		
+		User admin = new User("admin","password","javajigi","admin@hi.com");
+		User user1 = new User("user1","password","lee","lee@hi.com");
+		
+		userRepository.save(admin);
+		userRepository.save(user1);
+		
+	}
+
+	@Test(expected=CannotOperateException.class)
+	public void test_다른사람이쓴_댓글이_존재() throws Exception {
+		Question question = new Question(userRepository.findOne("admin"), "제목하나", "글하나");
+		questionRepository.save(question);
+		answerRepository.save(new Answer(userRepository.findOne("user1"), "의견하나", question));
+		
+		qnaService.deleteQuestion(1, userRepository.findOne("admin"));
+	}
+	
+	@Test
+	public void test_삭제가능() throws Exception {
+		Question question = new Question(userRepository.findOne("admin"), "제목둘", "글둘");
+		questionRepository.save(question);
+		
+		qnaService.deleteQuestion(2, userRepository.findOne("admin"));
+	}
+
+}
--- src/test/java/core/test/UserRepositoryTest.java
+++ src/test/java/core/test/UserRepositoryTest.java
@@ -2,32 +2,31 @@ package core.test;
 
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import next.dao.UserRepository;
 import next.model.User;
 
-public class UserRepositoryTest extends IntegrationTest{
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
 	private Logger log = LoggerFactory.getLogger(UserRepositoryTest.class);
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
-	@Before
-	public void setup(){
-		userRepository.save(new User("admin", "password", "javajigi", "admin@gmail.com"));
-	}
-	
 	@Test
 	public void crud() throws Exception {
+		userRepository.save(new User("admin", "password", "javajigi", "admin@gmail.com"));
 		List<User> users = userRepository.findAll();
 		for (User user : users) {
 			log.debug("{}", user.toString());
 		}
 	}
-
 }
