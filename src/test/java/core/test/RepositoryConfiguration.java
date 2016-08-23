9870e93183092bf6e3768b713da6b19bcadc51a6
zzoon20 <zzoon20@218.38.137.28>
2016-08-23 10:22:36 +0900

    test code

--- src/test/java/core/test/RepositoryConfiguration.java
+++ src/test/java/core/test/RepositoryConfiguration.java
@@ -1,11 +1,12 @@
 package core.test;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SuppressWarnings("deprecation")
+@EnableAutoConfiguration
 @Configuration
 @EntityScan("next.model")
 @EnableJpaRepositories("next.dao")
--- src/test/java/core/test/UserRepositoryTest.java
+++ src/test/java/core/test/UserRepositoryTest.java
@@ -1,18 +1,33 @@
 package core.test;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import next.dao.UserRepository;
+import next.model.User;
 
 public class UserRepositoryTest extends IntegrationTest{
+	private Logger log = LoggerFactory.getLogger(UserRepositoryTest.class);
 	
 	@Autowired
 	private UserRepository userRepository;
 
+	@Before
+	public void setup(){
+		userRepository.save(new User("admin", "password", "javajigi", "admin@gmail.com"));
+	}
+	
 	@Test
-	public void crud() {
-		
+	public void crud() throws Exception {
+		List<User> users = userRepository.findAll();
+		for (User user : users) {
+			log.debug("{}", user.toString());
+		}
 	}
 
 }
